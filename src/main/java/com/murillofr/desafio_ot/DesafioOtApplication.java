20774dc579ef689c9bfc51cbb2d40c35fd3e5290
Murillo Falcão Rodrigues <murillo.falcao@gmail.com>
2021-01-03 16:18:38 -0300

    Projeto criado

---
+++ src/main/java/com/murillofr/desafio_ot/DesafioOtApplication.java
@@ -0,0 +1,13 @@
+package com.murillofr.desafio_ot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murillofr/desafio_ot/DesafioOtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murillofr.desafio_ot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
