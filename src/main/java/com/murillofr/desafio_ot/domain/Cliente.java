1bb9852945ea4c6f327ca02e66a8a383403a0472
Murillo Falcão Rodrigues <murillo.falcao@gmail.com>
2021-01-03 20:35:44 -0300

    Banco de dados MYSQL e criacao automatica da base de dados

--- src/main/java/com/murillofr/desafio_ot/domain/Cliente.java
+++ src/main/java/com/murillofr/desafio_ot/domain/Cliente.java
@@ -2,9 +2,17 @@ package com.murillofr.desafio_ot.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Integer id;
 	private String nome;
 	private String email;
@@ -87,5 +95,13 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", cpf=" + cpf + ", dtnasc=" + dtnasc
+				+ "]";
+	}
+	
+	
     
 }
